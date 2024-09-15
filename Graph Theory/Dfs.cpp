0f386ecb54ba7dddc6608853c1442e6c2fd0dcce
S S P Junak <137554575+Junak12@users.noreply.github.com>
2024-09-15 20:14:26 +0600

    Add files via upload

---
+++ Graph Theory/Dfs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFS(int node, const vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[node] = true;
+    cout << node << " ";
+
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            DFS(neighbor, adj, visited);
+        }
+    }
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 3};
+    adj[3] = {1, 2, 4};
+    adj[4] = {3};
+
+    vector<bool> visited(V, false);
+    cout << "DFS starting from node 0: ";
+    DFS(0, adj, visited);
+    return 0;
+}
