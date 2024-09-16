10577f2105da646b7c9a31004834d52dd5dba73c
S S P Junak <137554575+Junak12@users.noreply.github.com>
2024-09-17 00:08:59 +0600

    Update Taking_string_as_node_and_edge.cpp

--- Graph Theory/Taking_string_as_node_and_edge.cpp
+++ Graph Theory/Taking_string_as_node_and_edge.cpp
@@ -1,1 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define opt() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+
+// 
+
+/*const int mx = 1e5 + 123; 
+vector<int> adj[mx];
+vector<bool> vis(mx, false);
+vector<bool> recstack(mx, false); // recursion stack*/
+
+map<string, vector<string>> adj;
+map<string, bool> visited, recStack;
+
+/*bool dfs(const string& u) {
+    visited[u] = true;
+    recStack[u] = true;
+    for (auto d : adj[u]) {
+        if (!visited[d]) {
+            if (dfs(d)) {
+                return true;
+            }
+        } else if (recStack[d]) {
+            return true;
+        }
+    }
+    recStack[u] = false;
+    return false;
+}*/
+
+int main() {
+    opt();
+    
+    int t;
+    cin >> t;
+
+    int case_num = 1;
+    while (t--) {
+        int m;
+        cin >> m;
+
+        adj.clear();
+        visited.clear();
+        recStack.clear();
+
+        set<string> st;
+        for (int i = 0; i < m; ++i) {
+            string a, b;
+            cin >> a >> b;
+            adj[a].push_back(b);
+            st.insert(a);
+            st.insert(b);
+        }
+
+        /*bool ok = false;
+        for (auto d : st) {
+            if (!visited[d]) {
+                if (dfs(d)) {
+                    ok = true;
+                    break;
+                }
+            }
+        }
+
+        if (ok) {
+            cout << "Case " << case_num << ": No" << endl;
+        } 
+        else {
+            cout << "Case " << case_num << ": Yes" << endl;
+        }
+
+        case_num++;*/
+    }
+
+    return 0;
+}
 
