01df0fd2675c93f01d577a94868bc5691e3528ae
S S P Junak <137554575+Junak12@users.noreply.github.com>
2024-09-19 01:32:33 +0600

    Update Is_biocolourable.cpp

--- Graph Theory/Is_biocolourable.cpp
+++ Graph Theory/Is_biocolourable.cpp
@@ -7,23 +7,23 @@ using namespace std;
 #define ld long double
 #define ull unsigned long long
 
-const int mx = 1e5 + 123;
+const int mx = 2e5 + 123;
 vector<int> adj[mx];
 vector<int> col(mx, -1);
 
-bool is_bicolourable(int u) {
-    col.assign(mx, -1);
-    col[u] = 1;
+bool bfs(int start) {
     queue<int> q;
-    q.push(u);
+    col[start] = 0;
+    q.push(start);
     while (!q.empty()) {
-        int cur = q.front();
+        int u = q.front();
         q.pop();
-        for (auto d : adj[cur]) {
-            if (col[d] == -1) {  
-                col[d] = 3 - col[cur];
+        for (auto d : adj[u]) {
+            if (col[d] == -1) {
+                col[d] = 1 - col[u];
                 q.push(d);
-            } else if (col[d] == col[cur]) { 
+            } 
+            else if (col[d] == col[u]) {
                 return false;
             }
         }
@@ -33,33 +33,31 @@ bool is_bicolourable(int u) {
 
 int main() {
     opt();
-    int t;
-    t = 1;
-
-    while (t--) {
-        int n;
-        cin >> n;
-        for (int i = 0; i < n; ++i) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        
-        bool is_possible = true;
-        for (int i = 0; i < n; ++i) {
-            if (col[i] == -1) { 
-                if (!is_bicolourable(i)) {
-                    is_possible = false;
-                    break;
-                }
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bool ok = true;
+    for (int i = 1; i <= n; ++i) {
+        if (col[i] == -1) {
+            if (!bfs(i)) {
+                ok = false;
+                break;
             }
         }
-        
-        if (is_possible) {
-            cout << "YES" << endl;
-        } else {
-            cout << "NO" << endl;
-        }
     }
+    /*for (int i = 1; i <= n; ++i) {
+        cout << col[i] << endl;
+    }*/
+    if (ok) {
+        cout << "Graph is Bicolored" << endl;
+    } 
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
 }
